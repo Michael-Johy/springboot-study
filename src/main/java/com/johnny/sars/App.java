874595dfb8b48dbf71cb2267e9195804c6a33f7a
yangtao3 <yangtao3@kingsoft.com>
2019-09-02 17:31:25 +0800

    springboot-study

---
+++ src/main/java/com/johnny/sars/App.java
@@ -0,0 +1,13 @@
+package com.johnny.sars;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
